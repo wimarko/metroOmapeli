7806d625186bb5a5059a91c012522804ac059733
wimarko <marko.wiik@metropolia.fi>
2022-12-30 12:35:15 +0200

    esineitä ilmestyy ja katoaa kentässä

--- Assets/Scripts/SceneSettings.cs
+++ Assets/Scripts/SceneSettings.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class SceneSettings : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class SceneSettings : MonoBehaviour
 
     [SerializeField] GameObject[] dropPoints;
     [SerializeField] GameObject[] dropObjects;
+    [SerializeField] float droptimer = 20f;
+    [SerializeField] float lifetime = 2f;
+    float counter;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,28 @@ public class SceneSettings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(counter < droptimer)
+        {
+            counter += Time.deltaTime;
+            
+        }
+        else
+        {
+            SpawnObject();
+            counter = 0;
+        }
+    }
+
+    public void SpawnObject()
+    {
+        Debug.Log("Spawnataab objekti");
+        int num = Random.Range(0, dropObjects.Length);
+        Debug.Log("randomi " + num);
+
+        int dropIndex = Random.Range(0, dropPoints.Length);
+
+        GameObject droppedObject = Instantiate(dropObjects[num], dropPoints[dropIndex].transform.position, dropPoints[dropIndex].transform.rotation);
+
+        Destroy(droppedObject, lifetime);
     }
 }
